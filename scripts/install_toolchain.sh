5f5800cb24c3ae1c71d28c57bd674d60cf70dad3
microbit-carlos <carlos@microbit.org>
2018-12-13 15:03:42 +0000

    Update pip and pipenv version, fix check for pipfile.lock

--- scripts/install_toolchain.sh
+++ scripts/install_toolchain.sh
@@ -3,6 +3,7 @@ alias pretty_echo='{ set +x; } 2> /dev/null; f(){ echo "#\n#\n# $1\n#\n#"; set -
 
 pretty_echo "Ensure Ubuntu has basic tools..."
 apt-get update -qq
+# software-properties-common needed to easily add PPAs
 apt-get install -y software-properties-common
 apt-get install -y git
 
@@ -13,12 +14,12 @@ add-apt-repository -y ppa:team-gcc-arm-embedded/ppa
 apt-get update -qq
 apt-get install -y gcc-arm-embedded=7-2018q2-1~bionic1
 
-pretty_echo "Installing pip v18.0 in system Python 2..."
+pretty_echo "Installing pip v18.1 in system Python 2..."
 apt-get install -y python-pip
-pip install --upgrade pip==18.0
+pip install --upgrade pip==18.1
 
-pretty_echo "Installing Pipenv v2018.7.1..."
-python -m pip install pipenv==2018.7.1
+pretty_echo "Installing Pipenv v2018.11.26..."
+python -m pip install pipenv==2018.11.26
 
 pretty_echo "Installing yotta dependencies..."
 apt-get install -y cmake=3.10.2-1ubuntu2
@@ -27,7 +28,7 @@ apt-get install -y ninja-build=1.8.2-1
 apt-get install -y srecord=1.58-1.1ubuntu2
 
 pretty_echo "Installing Yotta in system Python 2 via locked Pipfile"
-if [[ ! -f Pipfile.lock ]] ; then
+if [ ! -f Pipfile.lock ] ; then
     echo 'Pipfile.lock not found, exit.'
     exit 1
 fi
